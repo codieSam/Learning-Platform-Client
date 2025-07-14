2ff2561d0361e640f941ff4eedb9ad0a16294b37
codieSam <samratbelbase408@gmail.com>
2025-07-14 21:05:12 +0545

    Created and understood about @reduxjs/toolkit

---
+++ src/app/about/[id]/page.tsx
@@ -0,0 +1,28 @@
+
+"use client"
+
+function AboutId(){
+    return(
+        <h1>ABout Id</h1>
+    )
+}
+
+export default AboutId
+
+
+/*
+
+ next js ma jun function default hunx  
+  --> server component hunxa ---> 
+server sde rendering hunxa --> 
+(SEO huncha, browser level features , hooks haru use garnu mildaina)
+
+
+react js ma client component hunxa
+ --->  client side rendering huncha 
+ ---> Browser level features hunxa, 
+ hooks, dom haru use ganu milxa
+
+Server component lai client side component banauna milxa
+Tesko lagi mathi top ma ("use client") diney but seo ranking hudaina tespoxi  
+*/
