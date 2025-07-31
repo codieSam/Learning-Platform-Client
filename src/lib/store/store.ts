2599212cdf9f4d188fb5b419105469a7330eac18
codieSam <samratbelbase408@gmail.com>
2025-07-31 23:12:41 +0545

    more

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,13 +1,20 @@
 // collects all slices in store
 
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./auth/authSlice";
+import instituteSlice from "./institute/instituteSlice";
+import teacherSlice from "./teacher/teacherSlice";
 
 const store = configureStore({
     reducer: {
-      
+      auth: authSlice,
+      institute: instituteSlice,
+      teahcer: teacherSlice
     }
 })
 
 export default store
+//dispatch ko type poxi kam lagxa hamilai
+export type AppDispatch = typeof store.dispatch    //useDispatch lai 
+export type RootState  = ReturnType<typeof store.getState>  //useSelector lai
 
-export type AppDispatch = typeof store.dispatch
